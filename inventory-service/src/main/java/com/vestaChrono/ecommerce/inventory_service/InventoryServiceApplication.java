87acba88fc109b7486ced79d4f96068149022c47
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-10-01 19:55:11 +0530

    1.0 Ecommerce Initialization

---
+++ inventory-service/src/main/java/com/vestaChrono/ecommerce/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vestaChrono.ecommerce.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}

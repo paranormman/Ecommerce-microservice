46bbb385019fa2d248eb610ac126618741f9239f
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-10-02 14:29:16 +0530

    2.1 configuring eureka client on inventory service

--- inventory-service/src/main/java/com/vestaChrono/ecommerce/inventory_service/config/AppConfig.java
+++ inventory-service/src/main/java/com/vestaChrono/ecommerce/inventory_service/config/AppConfig.java
@@ -3,13 +3,19 @@ package com.vestaChrono.ecommerce.inventory_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
-public class MapperConfig {
+public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder().build();
     }
 
 }
